55f757675c83159151fc70e24405572bc7a94e4c
gowthamchi <130386327+gowthamchi@users.noreply.github.com>
2025-07-08 11:07:39 +0530

    Update create-pipeline.groovy

--- jenkins/init.groovy.d/create-pipeline.groovy
+++ jenkins/init.groovy.d/create-pipeline.groovy
@@ -1,30 +1,23 @@
-#!groovy
-
 import jenkins.model.*
 import hudson.model.*
-import hudson.security.*
+import hudson.model.Cause
 
 def instance = Jenkins.getInstance()
-
-// Create admin user
-def hudsonRealm = new HudsonPrivateSecurityRealm(false)
-hudsonRealm.createAccount("admin", "admin123")
-instance.setSecurityRealm(hudsonRealm)
-
-def strategy = new FullControlOnceLoggedInAuthorizationStrategy()
-instance.setAuthorizationStrategy(strategy)
-
-instance.save()
-
-// Create a Freestyle job named 'my-freestyle-job'
 def jobName = "my-freestyle-job"
 
 if (instance.getItem(jobName) == null) {
     def job = instance.createProject(FreeStyleProject, jobName)
 
-    // Optional: Add a simple shell build step
-    def builder = new hudson.tasks.Shell("echo Hello from Freestyle Job!")
-    job.getBuildersList().add(builder)
+    // Add a basic shell command
+    def shellStep = new hudson.tasks.Shell("echo Hello from Freestyle Job!")
+    job.getBuildersList().add(shellStep)
 
+    // Enable concurrent/manual builds
+    job.setConcurrentBuild(true)
+
+    // Save the job config
     job.save()
+
+    // Trigger the first build automatically
+    job.scheduleBuild(new Cause.UserIdCause())
 }
