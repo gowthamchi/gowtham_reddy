ddc562dd29dc684399dad59612eaa95879151ccc
gowthamchi <130386327+gowthamchi@users.noreply.github.com>
2025-07-08 10:09:17 +0530

    Update create-pipeline.groovy

--- jenkins/init.groovy.d/create-pipeline.groovy
+++ jenkins/init.groovy.d/create-pipeline.groovy
@@ -1,14 +1,14 @@
 #!groovy
 
 import jenkins.model.*
+import hudson.model.*
 import hudson.security.*
-import org.jenkinsci.plugins.workflow.job.WorkflowJob
-import org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition
+
+def instance = Jenkins.getInstance()
 
 // Create admin user
-def instance = Jenkins.getInstance()
 def hudsonRealm = new HudsonPrivateSecurityRealm(false)
-hudsonRealm.createAccount("admin", "admin123") // Username: admin, Password: admin123
+hudsonRealm.createAccount("admin", "admin123")
 instance.setSecurityRealm(hudsonRealm)
 
 def strategy = new FullControlOnceLoggedInAuthorizationStrategy()
@@ -16,25 +16,15 @@ instance.setAuthorizationStrategy(strategy)
 
 instance.save()
 
-// Create pipeline job named "my-pipeline"
-def jobName = "my-pipeline"
+// Create a Freestyle job named 'my-freestyle-job'
+def jobName = "my-freestyle-job"
+
 if (instance.getItem(jobName) == null) {
-    def pipelineJob = instance.createProject(WorkflowJob, jobName)
+    def job = instance.createProject(FreeStyleProject, jobName)
 
-    def jenkinsfile = new File("/var/jenkins_home/Jenkinsfile")
-    def pipelineScript = jenkinsfile.exists() ? jenkinsfile.text : """
-        pipeline {
-            agent any
-            stages {
-                stage('Default') {
-                    steps {
-                        echo 'Hello from default Jenkinsfile!'
-                    }
-                }
-            }
-        }
-    """
+    // Optional: Add a simple shell build step
+    def builder = new hudson.tasks.Shell("echo Hello from Freestyle Job!")
+    job.getBuildersList().add(builder)
 
-    pipelineJob.setDefinition(new CpsFlowDefinition(pipelineScript, true))
-    pipelineJob.save()
+    job.save()
 }
